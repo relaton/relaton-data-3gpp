2f326995b064b0e3ec2b180fa32338beaa7f75ae
Alexande B <abobrikovich@gmail.com>
2023-03-13 11:39:10 +0100

    Allow force option to be passed with workflow_dispatch

--- crawler.rb
+++ crawler.rb
@@ -2,10 +2,16 @@
 
 require 'fileutils'
 
+mode = ARGV.shift || ""
+
+unless mode.empty?
+  mode = "-#{mode}"
+end
+
 # FileUtils.rm_rf('data')
 
 system("sudo apt-get install mdbtools")
 
-system("relaton fetch-data status-smg-3GPP")
+system("relaton fetch-data status-smg-3GPP#{mode}")
 
 system("git add current.yaml")
