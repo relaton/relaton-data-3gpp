4eb832c37fb34e8049663dd34d4f0fbabca18567
Andrei Kislichenko <android.2net@gmail.com>
2023-06-07 19:55:14 -0400

    Update crawler.rb

--- crawler.rb
+++ crawler.rb
@@ -7,6 +7,3 @@ mode = mode == "force" ? "-#{mode}" : ""
 
 require "relaton_3gpp"
 Relaton3gpp::DataFetcher.fetch("status-smg-3GPP#{mode}")
-
-system("zip index-v1.zip index-v1.yaml")
-system("git add current.yaml index-v1.zip index-v1.yaml")
